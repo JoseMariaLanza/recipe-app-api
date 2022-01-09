f69ac425f1b5db6c4fb9e7f1f037fe21d1fe7669
Jose Maria Lanza <lanza.josemaria@gmail.com>
2022-01-09 19:48:30 -0300

    Updated admin to support custom user model.

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,33 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+# underscore is the convention for converting strings to human readeable text
+from django.utils.translation import gettext as _
+# The underscore it makes easier configure more than one language
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (  # Define sections for fieldsets
+        # param1=Title of the section, param2=fields
+        (None, {'fields': ('email', 'password')}),
+        # The comma it's when you use one field this tell django that this it doesn't a string
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
+    add_fieldsets = (
+        (None, {
+            # classes assigned to the form
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
+
+#  Register in Django Admin
+admin.site.register(models.User, UserAdmin)
---
+++ app/core/test_admin.py
@@ -0,0 +1,48 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    # setUp() runs before every tests that we run
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@mail.com',
+            password='123456'
+        )
+        # force_login() comes from Client(), this allows to login without
+        # having to do it manually
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@mail.com',
+            password='123456',
+            name='Test user full name'
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        #  Updates the url in the function at reverse (dinamic url)
+        url = reverse('admin:core_user_changelist')
+        #  Test Client for HTTP GET to url
+        res = self.client.get(url)
+
+        #  assertContains() check if res contains the attributes of a given object
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        #  Creates the below url: /admin/core/user/{id}
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
