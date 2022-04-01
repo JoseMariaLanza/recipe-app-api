f1f72e7b3b902d7384d256a7b047868210b97e15
Jose Maria Lanza <lanza.josemaria@gmail.com>
2022-03-31 23:02:31 -0300

    Added recipe image upload feature.

--- app/app/settings.py
+++ app/app/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # 127.0.0.1:8000/media/
-MEDIA_URS = '/media/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = '/vol/web/media'
 STATIC_ROOT = '/vol/web/static'
